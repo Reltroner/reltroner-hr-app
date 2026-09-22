1be3588597ccfcb5602d51d5a218483dfc6aabf5
Rei Reltroner SK <rei.reltroner@skill-wanderer.com>
2026-09-22 21:18:20 +0700

    chore: upgrade framework to Laravel 13

--- config/cache.php
+++ config/cache.php
@@ -105,4 +105,16 @@ return [
 
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Serializable Classes
+    |--------------------------------------------------------------------------
+    |
+    | When set to false, cache stores will not automatically serialize or
+    | unserialize arbitrary PHP objects, mitigating object-injection vulnerabilities.
+    |
+    */
+
+    'serializable_classes' => false,
+
 ];
